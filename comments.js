ca81a7185b5b40da7b11fb970e902c8b105b5631
OhhAlice1995 <35797355+OhhAlice1995@users.noreply.github.com>
2023-12-20 09:51:31 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,7 @@
 // create web server
-
-// import modules
 const express = require('express');
 const router = express.Router();
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 const User = require('../models/user');
 const { isLoggedIn } = require('../middleware');
-    
