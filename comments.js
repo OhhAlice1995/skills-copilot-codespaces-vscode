8e9fc7c49182ce1196a65a6bee2cbd5b61a72546
OhhAlice1995 <35797355+OhhAlice1995@users.noreply.github.com>
2023-12-20 09:54:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,17 @@
-// create web server
+// create web server using express
 const express = require('express');
 const router = express.Router();
-const Comment = require('../models/comment');
-const Post = require('../models/post');
-const User = require('../models/user');
-const { isLoggedIn } = require('../middleware');
+// import the comments controller
+const commentsCtrl = require('../controllers/comments');
+
+// export the router
+module.exports = router;
+
+// define the routes
+router.get('/comments', commentsCtrl.index);
+router.get('/comments/new', commentsCtrl.new);
+router.get('/comments/:id', commentsCtrl.show);
+router.post('/comments', commentsCtrl.create);
+router.delete('/comments/:id', commentsCtrl.delete);
+router.get('/comments/:id/edit', commentsCtrl.edit);
+router.put('/comments/:id', commentsCtrl.update);
